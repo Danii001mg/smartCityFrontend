22c96ccbec3776e2f5d4c12f959ce092c6480850
ivyncm <ivancantalejo21@hotmail.com>
2023-01-29 18:58:52 +0100

    cambio

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -62,7 +62,6 @@ export default function Login() {
                 onFailure={onFailure}
                 cookiePolicy={"single_host_origin"}
                 isSignedIn={true}
-                useBrowser= {false}
               />
               {loginMessage}
             </CardText>
