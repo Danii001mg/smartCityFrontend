3c615a361b75f9232162a728c49f5913e91dc500
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2023-06-28 17:30:29 +0200

    algunos arreglos

--- src/utils/apicalls.js
+++ src/utils/apicalls.js
@@ -4,13 +4,7 @@ export {
     getAllAccidentes,
     addNewAccidente,
     getSingleAccidente,
-    //getMyBookmarks,
     deleteAccidente,
-    getAllMovies,
-    addNewBookmark,
-    getSingleMovie,
-    getMyBookmarks,
-    deleteBookmark
 }
 
 function getAllAccidentes() {
@@ -23,35 +17,8 @@ function addNewAccidente(accidente){
 
 function getSingleAccidente(idaccidente) {
     return API.get('/accidentes/'+idaccidente).then(res => res.data);
- }
-
-// function getMyBookmarks(email) {
-//    return API.get('/bookmarks/'+email).then(res => res.data);
-//}
+}
 
 function deleteAccidente(idaccidente) {
-    return API.delete('/bookmarks/'+idaccidente).then(result => result.data);
-}
-
-
-function getAllMovies() {
-    return API.get('/movies').then(res => res.data);
-}
-
-function addNewBookmark(email, movie){
-    return API.post('/bookmarks', {
-        email,
-        movie}).then(result => result.data);
-}
-
-function getSingleMovie(idmovie) {
-    return API.get('/movies/'+idmovie).then(res => res.data);
- }
-
- function getMyBookmarks(email) {
-    return API.get('/accidentes/'+email).then(res => res.data);
-}
-
-function deleteBookmark(idbookmark) {
-    return API.delete('/bookmarks/'+idbookmark).then(result => result.data);
+    return API.delete('/accidentes/'+idaccidente).then(result => result.data);
 }
